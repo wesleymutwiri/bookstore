4b9caf020f2775fdaf09a89111981f31226f8622
wesleymutwiri <wesleymochiemo@gmail.com>
2019-07-04 15:26:36 +0300

    used unittest for front end testing

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,17 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
+class NewVisitorTest(unittest.TestCase):
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+	
+	def tearDown(self):
+		self.browser.quit()
+	
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		self.browser.get('http://localhost:8000')
+		self.assertIn('BookWorm', self.browser.title)
+		self.fail("Finished the test!")
 
-assert 'Django' in browser.title
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
