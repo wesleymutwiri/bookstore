ef50497c8701146045a6ca8d98d9f63b83c60e21
wesleymutwiri <wesleymochiemo@gmail.com>
2019-07-07 17:56:56 +0300

    unstaged and commited work

---
+++ bookworm/forms.py
@@ -0,0 +1,12 @@
+from django import forms 
+from .models import *
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        exclude = ['uploaded_date']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['']
